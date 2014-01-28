70c4b26ac5f0c180e5727466f4e04e3e82c359b6
Guimove22 <guillaume.fronczak@gmail.com>
2014-01-28 11:30:56 +0100

    Update InterfaceGraphique.java
    MaJ pour l'échiquier 4 4

--- src/miage/projet/reines/InterfaceGraphique.java
+++ src/miage/projet/reines/InterfaceGraphique.java
@@ -15,6 +15,7 @@ Created by Guillaume helped by Clément on 25/01/2014. (=
 
 package miage.projet.reines;
 
+import javax.swing.ImageIcon;
 import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
@@ -57,68 +58,68 @@ public class InterfaceGraphique extends JFrame implements ActionListener {
     
     // -- MENU -- //
     
-    private JMenuBar menuBar = new JMenuBar();
-    private JMenu Fichier = new JMenu("Fichier");
-
-    // Items de Fichier
-    private JMenuItem itemQuitter = new JMenuItem ("Quitter");
-   
-
-	//private Panneau MonPanneau = new Panneau();
-	private JPanel container = new JPanel();
-
+	    private JMenuBar menuBar = new JMenuBar();
+	    private JMenu Fichier = new JMenu("Fichier");
 	
-	// Classe Bouton dérivée de JButton
-    public class Bouton extends JButton implements MouseListener
-    {
-    	  private String name;
-    	  
-    	  public Bouton(String str)
-    	  {
-    	    super(str);
-    	    this.name = str;
-    	    this.addMouseListener(this);
-    	  }
-    	  
-    	  public void mouseClicked(MouseEvent e) {    
-          }
-    		 
-    	  public void mouseEntered(MouseEvent e) {
-    		  // Change la couleur du texte du bouton au survol
-    		  Bouton source = (Bouton)e.getSource();
-			  source.setForeground(Color.blue);
-		  }
-		 
-		  public void mouseExited(MouseEvent e) {
-    		  // Change la couleur du texte du bouton à la sortie de survol
-			  Bouton source = (Bouton)e.getSource();
-			  source.setForeground(Color.BLACK);
-		  }
-		 
-		  public void mousePressed(MouseEvent e) {
-		  }
-		  
-		  public void mouseReleased(MouseEvent event) {             
-		  } 
-    }
+		    // Items de Fichier
+		    private JMenuItem itemQuitter = new JMenuItem ("Quitter");
+		   
+		    
+		private JPanel container = new JPanel();
 
-    
-    // Création des boutons
-    private Bouton boutonStart = new Bouton("Start");
-    private Bouton boutonReset = new Bouton("Reset");
-    private Bouton boutonStop = new Bouton("Stop");
-    private Bouton boutonPlayBack = new Bouton("Play Back");
-    private Bouton boutonEnd = new Bouton("End");
-    private Bouton boutonQuitter = new Bouton("Quitter");
-    
-    private Bouton boutonCreerGrille = new Bouton("Créer Grille");
-    
-    // ComboBox pour le choix de la grille
-    private JComboBox cbxChoixEchiquier = new JComboBox();
+	// -- LES BOUTONS -- //
+	
+		// Classe Bouton dérivée de JButton
+	    public class Bouton extends JButton implements MouseListener
+	    {
+	    	  private String name;
+	    	  
+	    	  public Bouton(String str)
+	    	  {
+	    	    super(str);
+	    	    this.name = str;
+	    	    this.addMouseListener(this);
+	    	  }
+	    	  
+	    	  public void mouseClicked(MouseEvent e) {    
+	          }
+	    		 
+	    	  public void mouseEntered(MouseEvent e) {
+	    		  // Change la couleur du texte du bouton au survol
+	    		  Bouton source = (Bouton)e.getSource();
+				  source.setForeground(Color.blue);
+			  }
+			 
+			  public void mouseExited(MouseEvent e) {
+	    		  // Change la couleur du texte du bouton à la sortie de survol
+				  Bouton source = (Bouton)e.getSource();
+				  source.setForeground(Color.BLACK);
+			  }
+			 
+			  public void mousePressed(MouseEvent e) {
+			  }
+			  
+			  public void mouseReleased(MouseEvent event) {             
+			  } 
+	    }
+	
+	    
+	    // Création des boutons
+	    private Bouton boutonStart = new Bouton("Start");
+	    private Bouton boutonReset = new Bouton("Reset");
+	    private Bouton boutonStop = new Bouton("Stop");
+	    private Bouton boutonPlayBack = new Bouton("Play Back");
+	    private Bouton boutonEnd = new Bouton("End");
+	    private Bouton boutonQuitter = new Bouton("Quitter");
+	    private Bouton boutonCreerGrille = new Bouton("Créer Grille");
+	    
+	    // ComboBox pour le choix de la grille
+	    private JComboBox cbxChoixEchiquier = new JComboBox();
     
     
     // -- LES ACTIONS -- //
-    public void actionPerformed(ActionEvent arg0) 
+	    
+	    public void actionPerformed(ActionEvent arg0) 
     {
     	// Bouton Quitter
     	if (arg0.getSource() == boutonQuitter)
@@ -131,6 +132,7 @@ public class InterfaceGraphique extends JFrame implements ActionListener {
     	    	System.exit(0);
     	    }
     	}    
+    	
     	// Bouton pour redéfinir la taille de l'échiquier
     	else if (arg0.getSource() == boutonCreerGrille)
     	{
@@ -149,7 +151,7 @@ public class InterfaceGraphique extends JFrame implements ActionListener {
     				a = a==1 ? 0 : 1;
     				for(int colonne=0;colonne<6;colonne++){
     					tab[colonne][ligne]= new JLabel();
-    					//tab[colonne][ligne].setIcon(new ImageIcon("queen.png"));
+    					tab[colonne][ligne].setIcon(new ImageIcon("queen.png"));
     					if((colonne+1)%2==a) tab[colonne][ligne].setBackground(Color.white);
     					else tab[colonne][ligne].setBackground(Color.black);
     					tab[colonne][ligne].setOpaque(true);
@@ -172,7 +174,7 @@ public class InterfaceGraphique extends JFrame implements ActionListener {
         				a = a==1 ? 0 : 1;
         				for(int colonne=0;colonne<4;colonne++){
         					tab[colonne][ligne]= new JLabel();
-        					//tab[colonne][ligne].setIcon(new ImageIcon("queen.png"));
+        					tab[colonne][ligne].setIcon(new ImageIcon("Users/Guimove/Documents/workspace/Queen.png"));
         					if((colonne+1)%2==a) tab[colonne][ligne].setBackground(Color.white);
         					else tab[colonne][ligne].setBackground(Color.black);
         					tab[colonne][ligne].setOpaque(true);
@@ -194,7 +196,7 @@ public class InterfaceGraphique extends JFrame implements ActionListener {
         				a = a==1 ? 0 : 1;
         				for(int colonne=0;colonne<8;colonne++){
         					tab[colonne][ligne]= new JLabel();
-        					//tab[colonne][ligne].setIcon(new ImageIcon("queen.png"));
+        					tab[colonne][ligne].setIcon(new ImageIcon("queen.png"));
         					if((colonne+1)%2==a) tab[colonne][ligne].setBackground(Color.white);
         					else tab[colonne][ligne].setBackground(Color.black);
         					tab[colonne][ligne].setOpaque(true);
@@ -209,12 +211,12 @@ public class InterfaceGraphique extends JFrame implements ActionListener {
 	}
 
  
-	// Constructeur de l'interface Graphique
+	// -- CONSTRUCTEUR -- //
+	    
 		public InterfaceGraphique()
 		{
 			
-			// --- FENETRE PRINCIPALE / MISE EN FORME  --- //
-			
+			// --- FENETRE PRINCIPALE --- //
 			
 			// -- JFRAME -- //
 			
@@ -230,18 +232,23 @@ public class InterfaceGraphique extends JFrame implements ActionListener {
 			MaFenetre.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
 			
 			
+			
+			// --- ECHIQUIER --- //
+			
 			// -- JPANEL -- //
 			    
-			
 		    // Définition du container et des Layout
 			container.setLayout(new GridLayout(8,8));
 			JLabel tab[][]=new JLabel[8][8];
 			int a=1;
-			for(int ligne=0;ligne<8;ligne++){
+			// On construit l'échiquier, on rempli chaque case du GridLayout par un JLabel noir ou blanc
+			for(int ligne=0;ligne<8;ligne++)
+			{
 				a = a==1 ? 0 : 1;
-				for(int colonne=0;colonne<8;colonne++){
+				for(int colonne=0;colonne<8;colonne++)
+				{
 					tab[colonne][ligne]= new JLabel();
-					//tab[colonne][ligne].setIcon(new ImageIcon("queen.png"));
+					tab[colonne][ligne].setIcon(new ImageIcon("queen.png"));
 					if((colonne+1)%2==a) tab[colonne][ligne].setBackground(Color.white);
 					else tab[colonne][ligne].setBackground(Color.black);
 					tab[colonne][ligne].setOpaque(true);
@@ -252,17 +259,16 @@ public class InterfaceGraphique extends JFrame implements ActionListener {
 				
 		  
 		    
-		    // -- BOUTONS -- //
+		    // -- BOUTONS  ET COMBOBOX -- //
 	
 		    // On rempli la combo box
 		    cbxChoixEchiquier.addItem(4);
 		    cbxChoixEchiquier.addItem(6);
 		    cbxChoixEchiquier.addItem(8);
-		    
-		    // Placement des boutons
+		   
+		    // On ajoute la combo box et les boutons
 		    JPanel south = new JPanel();
-		    
-		    // On ajoute la combo box
+		   
 		    south.add(cbxChoixEchiquier);
 		    south.add(boutonCreerGrille);
 		    south.add(boutonStart);
@@ -272,6 +278,7 @@ public class InterfaceGraphique extends JFrame implements ActionListener {
 		    south.add(boutonEnd);
 		    south.add(boutonQuitter);
 		   
+		    // On ajoute tout ça à la fenêtre principale
 		    MaFenetre.add(south, BorderLayout.SOUTH);
 		    MaFenetre.add(container, BorderLayout.CENTER);
 		    
@@ -302,15 +309,12 @@ public class InterfaceGraphique extends JFrame implements ActionListener {
 		    this.menuBar.add(Fichier);
 		    this.setJMenuBar(menuBar);
 			
-		  
-		  
 		    // Rendre la fenêtre visible
 		    MaFenetre.setVisible(true);
-		    
-		   
+		    	   
 		}
 		
-		// Main
+		// --- MAIN --- //
 		 public static void main(String[] args)
 		 {       
 			 InterfaceGraphique fenetre = new InterfaceGraphique();
